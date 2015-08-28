dd73fce9dfdf638c779cfcc17901cc35a0ae52a3
Tiago Butzke <tiago.butzke@gmail.com>
2015-08-28 09:55:21 -0300

    removing nocache parameter

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@
  * Created by tiagobutzke on 6/30/15.
  */
 var config = {
-    javascript_url: "https://secure.mlstatic.com/sdk/javascript/v1/mercadopago.js?nocache=" + Math.random() * 10,
+    javascript_url: "https://secure.mlstatic.com/sdk/javascript/v1/mercadopago.js",
     public_key: {
         test: "TEST-c24e58b1-a4e0-40f6-adb4-a6ca7cf60209",
         live: "APP_USR-41ff8f64-11f2-47d7-91fb-ca4db9086c15"
