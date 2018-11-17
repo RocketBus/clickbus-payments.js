62a238270674942431109f0c20ff5449e0c3c68c
David Tavares <david.tavares@clickbus.com>
2018-11-17 12:12:09 -0200

    Adding Paymee payment gateway

---
+++ src/gateways/paymee.js
@@ -0,0 +1,11 @@
+"use strict";
+
+function EletronicFundsTransfer() {
+    this.type = 'eletronic_funds_transfer';
+    this.name = 'paymee';
+}
+
+EletronicFundsTransfer.prototype.start = function() { };
+EletronicFundsTransfer.prototype.clearSession = function() { };
+EletronicFundsTransfer.prototype.addChildPublicKey = function() { };
+EletronicFundsTransfer.prototype.createToken = function() { };
