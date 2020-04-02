d4fdad98b1590f3c00443bc9cad00f6cdd7611a5
Pietro Verdile <pietro.verdile@clickbus.com>
2020-04-02 18:08:35 -0300

    [PP-207] Add Wallet MercadoPago gateway

---
+++ src/gateways/wallet.js
@@ -0,0 +1,11 @@
+"use strict";
+
+function Wallet() {
+    this.type = 'wallet';
+    this.name = 'wallet_mercadopago';
+}
+
+Wallet.prototype.start = function() { };
+Wallet.prototype.clearSession = function() { };
+Wallet.prototype.addChildPublicKey = function() { };
+Wallet.prototype.createToken = function() { };
